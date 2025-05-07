b0e3abfae5df68029c9a6f09fa8810c4b0803445
perls <info@megalithic.me>
2025-05-07 14:44:27 -0700

    commit before master branch push

--- src/bin/make_invoice.rs
+++ src/bin/make_invoice.rs
@@ -1,4 +1,3 @@
-
 use ldk_node::lightning_invoice::{Bolt11InvoiceDescription, Description};
 use megalith_lsps2::setup_node;
 
@@ -16,13 +15,13 @@ fn main() {
     );
     let invoice = node
         .bolt11_payment()
-        .receive_via_jit_channel(10_000, &desc, 3_600, None)
+        .receive(10_000_000, &desc, 3_600)
         .unwrap();
 
     println!("INVOICE for 10,000 satoshis:\n{invoice}\n");
     println!("Logs ➜ {log_path}");
 
-    // keep running so the LSP can actually open the channel
+    // keep running to allow invoice to be paid
     loop {
         std::thread::sleep(std::time::Duration::from_secs(60));
     }
